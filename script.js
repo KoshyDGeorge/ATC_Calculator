3637c724c4adc76ef538b7c62b238c2b321373a0
KoshyDGeorge <157341037+KoshyDGeorge@users.noreply.github.com>
2024-01-24 22:49:45 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,47 +1,28 @@
-
-let trigNumber;
 let CurrentExpresstion;
-
-
-let operation;
 let result = 0;
-
-let trigNumArray =[];
 let CurrentExpresstionArray =[];
 
 const display = document.getElementById('display')
 const output = document.getElementById('displayOutput')
 
 function getNumber(Num){
-    
-    
-    if(CurrentExpresstionArray[CurrentExpresstionArray.length - 2] === ')' ){
-        getOperator('*');
-    }
     CurrentExpresstionArray.push(Num);
     CurrentExpresstion = CurrentExpresstionArray.join('');
     display.value=CurrentExpresstion;
 }
 
 function getOperator(operator){
-    if(operator === "^"){
-        CurrentExpresstionArray.push(operator);
-        CurrentExpresstion = CurrentExpresstionArray.join('');
-        display.value=CurrentExpresstion;
-    }else{
-        CurrentExpresstionArray.push(' ');
-        CurrentExpresstionArray.push(operator);
-        CurrentExpresstionArray.push(' ');
-        CurrentExpresstion = CurrentExpresstionArray.join('');
-        display.value=CurrentExpresstion;
-    }
+    CurrentExpresstionArray.push(' ');
+    CurrentExpresstionArray.push(operator);
+    CurrentExpresstionArray.push(' ');
+    CurrentExpresstion = CurrentExpresstionArray.join('');
+    display.value=CurrentExpresstion;
     
 }
 
 function clearScreen(){
     display.value = 0;
     output.value =0;
-    operation=null;
     result=0;
     CurrentExpresstion = null
     CurrentExpresstionArray =[];    
@@ -61,98 +42,103 @@ function backspace(){
     display.value = CurrentExpresstion;
 }
 
+function resolveTrigFunction(expression,trigFunction){
+    let Regex;
+    let mathTrigFunction ;
+    if(trigFunction === 'Sin'){
+        Regex = /\bSin\s*(-?\d+(\.\d+)?)\b/;
+        mathTrigFunction = Math.sin;
+
+    }else if(trigFunction === 'Tan'){
+        Regex = /\bTan\s*(-?\d+(\.\d+)?)\b/;
+        mathTrigFunction = Math.tan;
+    }else{
+        Regex = /\bCos\s*(-?\d+(\.\d+)?)\b/;
+        mathTrigFunction = Math.cos;
+    }
+
+    
+    while(Regex.test(expression)){
+        const match = expression.match(Regex);
+        const [fullMatch, num1] = match;
+        const result = mathTrigFunction((parseFloat(num1) * Math.PI) / 180).toFixed(7);
+        expression = expression.replace(fullMatch, result);
+    }
+    return expression;
+
+}
+
+function evaluateParentheses(subExpression){
+
+    while(subExpression.includes('(')){
+        const openParenthesesIndex = subExpression.lastIndexOf('(');
+        const closeParenthesesIndex = subExpression.indexOf(')',openParenthesesIndex);
+        const innerExpression = subExpression.slice(openParenthesesIndex+1, closeParenthesesIndex);
+        const innerResult = evaluateExpression(innerExpression);
+        if(innerResult === 'broke'){
+            return 'broke';
+        }
+        subExpression = subExpression.slice(0, openParenthesesIndex) + innerResult + subExpression.slice(closeParenthesesIndex+1);
+        
+    }
+    return subExpression;
+    
+}
+
+function evaluateExpression(expression){
+
+    
+    expression = evaluateParentheses(expression);
+
+    expression = resolveTrigFunction(expression,'Sin');
+    expression = resolveTrigFunction(expression,'Cos');
+    expression = resolveTrigFunction(expression,'Tan');
+
+    
+    const exponentRegex = /(\d+(\.\d+)?([eE][+-]?\d+)?)[\s]*[\^][\s]*(\d+(\.\d+)?([eE][+-]?\d+)?)/    ;
+    while(exponentRegex.test(expression)){
+        const match = expression.match(exponentRegex);
+        const [fullMatch, number1, _, _1, number2] = match;
+        const result =  parseFloat(number1)**parseFloat(number2);
+        expression = expression.replace(fullMatch, result);
+    }
+
+    
+
+
+
+    const multiplyDivideRegex = /(-?\s*\d+(\.\d+)?([eE][+-]?\d+)?)[\s]*[\/\*][\s]*(-?\s*\d+(\.\d+)?([eE][+-]?\d+)?)/    ;
+    while(multiplyDivideRegex.test(expression)){
+        const match = expression.match(multiplyDivideRegex);
+        let [fullMatch, number1, _,_1, number2] = match;
+        number1=number1.replace(/ /g,'');
+        number2=number2.replace(/ /g,'');
+        const operator = fullMatch.includes("*") ? "*" : "/";
+        if(operator === "/" && parseFloat(number2)===0){
+            output.value = "Cannot Divide By Zero";
+            return 'broke';
+        }
+        const result = operator ==='*' ? parseFloat(number1)*parseFloat(number2) : parseFloat(number1)/parseFloat(number2);
+        expression = expression.replace(fullMatch, result);
+    }
+
+    const addSubtractRegex = /(\d+(\.\d+)?([eE][+-]?\d+)?)\s*[-+]\s*(\d+(\.\d+)?([eE][+-]?\d+)?)/    ;
+    while(addSubtractRegex.test(expression)){
+        const match = expression.match(addSubtractRegex);
+        const [fullMatch,number1,_,_1,number2] = match;
+        const operator = fullMatch[match.index + number1.length].trim();
+        const result = operator === '+' ? parseFloat(number1) + parseFloat(number2) : parseFloat(number1) - parseFloat(number2);
+        expression = expression.replace(fullMatch,result);
+    }
+    return parseFloat(expression);
+    
+    
+}
+
 
 function calculateExpression(){
     CurrentExpresstion = " "+CurrentExpresstion+" ";
-    function evaluateParentheses(subExpression){
-
-        while(subExpression.includes('(')){
-            const openParenthesesIndex = subExpression.lastIndexOf('(');
-            const closeParenthesesIndex = subExpression.indexOf(')',openParenthesesIndex);
-            const innerExpression = subExpression.slice(openParenthesesIndex+1, closeParenthesesIndex);
-            const innerResult = evaluateExpression(innerExpression);
-            if(innerResult === 'broke'){
-                return 'broke';
-            }
-            subExpression = subExpression.slice(0, openParenthesesIndex) + innerResult + subExpression.slice(closeParenthesesIndex+1);
-            
-        }
-        return subExpression;
-        
-    }
-
-    function evaluateExpression(expression){
-
-        
-        expression = evaluateParentheses(expression);
-        if(expression === 'broke'){
-            return 'broke';
-        }
-        //by the time it leaves this, the expression should not have any parentheses
-        const sinRegex = /\bSin\s*(-?\d+(\.\d+)?)\b/;
-        while(sinRegex.test(expression)){
-            const match = expression.match(sinRegex);
-            const [fullMatch, num1] = match;
-            const result = Math.sin((parseFloat(num1) * Math.PI) / 180);
-            expression = expression.replace(fullMatch, result);
-        }
-
-        const cosRegex = /\bCos\s*(-?\d+(\.\d+)?)\b/;
-        while(cosRegex.test(expression)){
-            const match = expression.match(cosRegex);
-            const [fullMatch, num1] = match;
-            const result = Math.cos( (parseFloat(num1) * Math.PI) / 180);
-            expression = expression.replace(fullMatch, result);
-        }
-
-        const tanRegex = /\bTan\s*(-?\d+(\.\d+)?)\b/;
-        while(tanRegex.test(expression)){
-            const match = expression.match(tanRegex);
-            const [fullMatch, num1] = match;
-            if((num1 * Math.PI) / 180 == 90){
-                output.value = 'Tan calculation Error';
-                return 'broke';
-            }
-            const result = Math.tan( (parseFloat(num1) * Math.PI) / 180);
-            expression = expression.replace(fullMatch, result);
-        }
-
-        
-        const exponentRegex = /(\d+(\.\d+)?)[\s]*[\^][\s]*(\d+(\.\d+)?)/;
-        while(exponentRegex.test(expression)){
-            const match = expression.match(exponentRegex);
-            const [fullMatch, num1, _, num2] = match;
-            const result =  parseFloat(num1)**parseFloat(num2);
-            expression = expression.replace(fullMatch, result);
-        }
-
-        const multiplyDivideRegex = /(-?\s*\d+(\.\d+)?)[\s]*[\/\*][\s]*(-?\s*\d+(\.\d+)?)/;
-        while(multiplyDivideRegex.test(expression)){
-            const match = expression.match(multiplyDivideRegex);
-            let [fullMatch, num1, _, num2] = match;
-            num1=num1.replace(/ /g,'');
-            num2=num2.replace(/ /g,'');
-            const operator = fullMatch.includes("*") ? "*" : "/";
-            if(operator === "/" && parseFloat(num2)===0){
-                output.value = "Cannot Divide By Zero";
-                return 'broke';
-            }
-            const result = operator ==='*' ? parseFloat(num1)*parseFloat(num2) : parseFloat(num1)/parseFloat(num2);
-            expression = expression.replace(fullMatch, result);
-        }
-
-        const addSubtractRegex = /(\d+(\.\d+)?)\s*[-+]\s*(\d+(\.\d+)?)/;
-        while(addSubtractRegex.test(expression)){
-            const match = expression.match(addSubtractRegex);
-            const [fullMatch,num1,_,num2] = match;
-            const operator = fullMatch[match.index + num1.length].trim();
-            const result = operator === '+' ? parseFloat(num1) + parseFloat(num2) : parseFloat(num1) - parseFloat(num2);
-            expression = expression.replace(fullMatch,result);
-        }
-        return parseFloat(expression);
-        
-        
-    }
+    
     const result = evaluateExpression(CurrentExpresstion);
     
     CurrentExpresstion = null
